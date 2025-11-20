677df47650a618086d865b1a3f2f6fc7aebbac99
rosh <rosh1234@gmail.com>
2025-11-20 10:44:24 +0530

    first commit

---
+++ ameer/ameer/src/main/java/com/example/ameer/AmeerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ameer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmeerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmeerApplication.class, args);
+	}
+
+}
---
+++ ameer/ameer/src/test/java/com/example/ameer/AmeerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ameer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmeerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
